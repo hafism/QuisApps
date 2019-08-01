2ee78d9078da633c1e232b33d0a2f57ffd0b2fb3
Muhammad Hafiz <32486739+hafism@users.noreply.github.com>
2019-08-01 09:45:10 +0700

    Add files via upload

---
+++ TubesAkb/app/src/main/java/com/hafis/quizqlite/Constant.java
@@ -0,0 +1,8 @@
+package com.hafis.quizqlite;
+
+public class Constant {
+
+    public static String BLOG = "https://wordpress.com/view/triksukseswirausaha.home.blog";
+    private static String DESIGNED_BY = "Designed by Muhammad Hafiz";
+    public static String SHARE_CONTENT = "Blog Muhammad Hafiz:\n" + BLOG + "\n- " + DESIGNED_BY;
+}
